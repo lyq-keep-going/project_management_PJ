4b17ab32ecf6112da4b6efd582f34d4349643705
bathtown <hyf-0418@outlook.com>
2021-11-20 08:25:11 +0800

    refine

--- web/README.md
+++ web/README.md
@@ -13,3 +13,4 @@ npm run dev
 1. [element](https://element-plus.gitee.io/zh-CN/)
 2. [vuejs](https://v3.cn.vuejs.org/)
 3. [vitejs](https://cn.vitejs.dev/)
+4. [Vue Router](https://next.router.vuejs.org/zh/)
--- web/happy_go/src/pages/PersonalCenter.vue
+++ web/happy_go/src/pages/PersonalCenter.vue
@@ -3,10 +3,10 @@
 
     <el-tabs type="card" @tab-click="handleClick" tab-position="left">
         <el-tab-pane label="收藏课程" id="FavoriteClass">收藏课程</el-tab-pane>
-        <el-tab-pane label="我的出售" id="Selling"></el-tab-pane>
-        <el-tab-pane label="我的帖子" id="Post"></el-tab-pane>
-        <el-tab-pane label="收藏物品" id="FavoriteGoods"></el-tab-pane>
-        <el-tab-pane label="我的消息" id="Message"></el-tab-pane>
+        <el-tab-pane label="我的出售" id="Selling">我的出售</el-tab-pane>
+        <el-tab-pane label="我的帖子" id="Post">我的帖子</el-tab-pane>
+        <el-tab-pane label="收藏物品" id="FavoriteGoods">收藏物品</el-tab-pane>
+        <el-tab-pane label="我的消息" id="Message">我的消息</el-tab-pane>
         <el-tab-pane label="我的团购" id="GroupPurchase">我的团购</el-tab-pane>
     </el-tabs>
 </template>
