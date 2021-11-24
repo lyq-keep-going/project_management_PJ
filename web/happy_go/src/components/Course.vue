735c2e0d90c0bd762395699234ac44fb396955f9
bathtown <hyf-0418@outlook.com>
2021-11-24 20:31:14 +0800

    add course component

---
+++ web/happy_go/src/components/Course.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-card class="courseCard" shadow="hover">
+        <img src="../assets/logo.png" />
+        <div>
+            <h3>课程</h3>
+            <p>课程简介</p>
+        </div>
+    </el-card>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+.courseCard {
+    width: 100%;
+    margin-top: 20px;
+}
+.courseCard img {
+    width: 80%;
+}
+</style>
