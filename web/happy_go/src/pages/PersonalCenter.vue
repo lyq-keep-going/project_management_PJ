19be087d55fead3da9246910090e45bf915af3e0
bathtown <hyf-0418@outlook.com>
2021-11-24 20:32:00 +0800

    update personalCenter page

--- web/happy_go/src/pages/PersonalCenter.vue
+++ web/happy_go/src/pages/PersonalCenter.vue
@@ -1,8 +1,8 @@
 <template>
-    <h1>个人中心</h1>
-
     <el-tabs type="card" @tab-click="handleClick" tab-position="left">
-        <el-tab-pane label="收藏课程" id="FavoriteClass">收藏课程</el-tab-pane>
+        <el-tab-pane label="收藏课程" id="FavoriteCourse">
+            <FavoriteCourse />
+        </el-tab-pane>
         <el-tab-pane label="我的出售" id="Selling">我的出售</el-tab-pane>
         <el-tab-pane label="我的帖子" id="Post">我的帖子</el-tab-pane>
         <el-tab-pane label="收藏物品" id="FavoriteGoods">收藏物品</el-tab-pane>
@@ -10,12 +10,18 @@
         <el-tab-pane label="我的团购" id="GroupPurchase">我的团购</el-tab-pane>
     </el-tabs>
 </template>
+
 <script lang="ts">
+import FavoriteCourse from "./personal/FavoriteCourse.vue"
+
 export default {
     data() {
         return {
             activeName: 'first',
         }
+    },
+    components: {
+        FavoriteCourse
     },
     methods: {
         handleClick(tab, event) {
