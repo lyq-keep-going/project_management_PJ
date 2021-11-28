adf59f593a178e146f18134c371c39cd46e624d1
罗宇琦 <1258648285@qq.com>
2021-11-28 16:35:06 +0800

    discussion GUI completed

---
+++ mini-project/PJmanagement/components/detailed_info/detailed_info.js
@@ -0,0 +1,42 @@
+// components/detailed_info/detailed_info.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        lessonName:{
+            type:String,
+            value:""
+        },
+        lessonId:{
+            type:String,
+            value:""
+        },
+        teacherName:{
+            type:String,
+            value:""
+        },
+        semester:{
+            type:String,
+            value:""
+        },
+        credit:{
+            type:Number,
+            value:0
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
