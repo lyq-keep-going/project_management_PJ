6aaad30a7a70b9a4ae7897570d91e6c5deabb049
罗宇琦 <1258648285@qq.com>
2021-11-28 15:17:43 +0800

    add components Tab_1 tag entry and plain_tag

---
+++ mini-project/PJmanagement/components/entry/entry.js
@@ -0,0 +1,30 @@
+// components/entry/entry.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        content:{
+            type:String,
+            value:"标题"
+        },
+        date:{
+            type:String,
+            value:"0000-00-00"
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
