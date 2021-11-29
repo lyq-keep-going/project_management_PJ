9a4fccb3adb1baaff9dcc73216a93b8768a8612c
罗宇琦 <1258648285@qq.com>
2021-11-29 10:27:38 +0800

    commidity_details completed

--- mini-project/PJmanagement/pages/commodity_detail/commodity_detail.js
+++ mini-project/PJmanagement/pages/commodity_detail/commodity_detail.js
@@ -5,7 +5,29 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        commodity_pic:[
+            'https://z3.ax1x.com/2021/11/26/oVEkK1.jpg',
+            'https://z3.ax1x.com/2021/11/26/oVKuwR.jpg',
+            'https://z3.ax1x.com/2021/11/26/oVKKT1.jpg'
+        ],
+        info:{
+            seller:"ahaah",
+            bookname:"《西游记》",
+            author:"施耐庵",
+            publisher:"商务印书馆",
+            new_degree:"全新",
+            price:58,
+            content:"多买了一本"
+        },
+        translate_dic:{
+            seller:"卖家",
+            bookname:"书名",
+            author:"作者",
+            publisher:"出版社",
+            new_degree:"成色",
+            price:"价格",
+            content:"描述"
+        }
     },
 
     /**
