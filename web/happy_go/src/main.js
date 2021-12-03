56441bb9e045e77c8a4a0769954ce9262e195b1c
bathtown <hyf-0418@outlook.com>
2021-12-03 09:20:46 +0800

    add axios & element-plus icons

--- web/happy_go/src/main.js
+++ web/happy_go/src/main.js
@@ -2,16 +2,24 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElIconModules from '@element-plus/icons'
+
 import App from './App.vue'
 import router from './router/router'
 
 import axios from 'axios'
-import VueAxios from 'vue-axios'
- 
+
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+for (const iconName in ElIconModules) {
+    if (Reflect.has(ElIconModules, iconName)) {
+        const item = ElIconModules[iconName];
+        app.component(iconName, item)
+    }
+}
+app.config.globalProperties.$axios = axios;
 
 // 最后再 mount
 app.mount('#app')
