c73aeb22f9c2a591beea1fc6732dd45633ba3c8d
bathtown <hyf-0418@outlook.com>
2021-12-03 09:21:05 +0800

    add component "物品"

---
+++ web/happy_go/src/components/Goods.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-card class="courseCard" shadow="hover">
+        <img src="../assets/logo.png" />
+        <div>
+            <h3>物品名</h3>
+            <p>物品描述</p>
+        </div>
+    </el-card>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+.courseCard {
+    width: 100%;
+    margin-top: 20px;
+}
+.courseCard img {
+    width: 80%;
+}
+</style>
