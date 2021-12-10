fa737f42eaf23d8b70e5e107d2a0d7c5d429b7f7
bathtown <hyf-0418@outlook.com>
2021-12-10 18:49:42 +0800

    finish message

--- web/happy_go/src/pages/personal/Message.vue
+++ web/happy_go/src/pages/personal/Message.vue
@@ -11,21 +11,46 @@
                     >{{ item.sender.username }}</el-badge>
                 </el-col>
                 <el-col :span="16">
-                    <p style="text-align: left;">{{ item.last_message.content }}</p>
+                    <p style="text-align: left;">
+                        {{ item.last_message.content }}
+                        <el-tag type="info">{{ item.last_message.issueTime }}</el-tag>
+                    </p>
                 </el-col>
                 <el-col :span="4">
-                    <el-tag>{{ item.last_message.issueTime }}</el-tag>
+                    <el-button
+                        type="primary"
+                        @click="dialogVisible = true, getDialogue(item.sender.username, item.sender.id, MpageNum, MpageSize)"
+                    >展开</el-button>
                 </el-col>
             </el-row>
         </el-card>
     </div>
-    <div class="block">
+
+    <el-dialog v-model="dialogVisible" title="对话框" width="80%" top="5vh" :before-close="clearM">
+        <div class="dialogue">
+            <ul v-infinite-scroll="handleMPageClick" class="infinite-list" style="overflow: auto">
+                <li v-for="item in Mlist" :key="item" class="infinite-list-item">
+                    <p>{{ item.senderId == receiverId ? MuserName : myName }}</p>
+                    <p>{{ item.content }}</p>
+                    <el-divider></el-divider>
+                </li>
+            </ul>
+        </div>
+
+        <el-input v-model="content" @keyup.enter.native="sendMessage">
+            <template #append>
+                <el-button type="primary" @click="sendMessage">发送</el-button>
+            </template>
+        </el-input>
+    </el-dialog>
+
+    <div>
         <el-pagination
             :page-size="pageSize"
             layout="prev, pager, next"
             :currentPage="pageNum"
             :total="total"
-            @current-change="handleCurrentChange"
+            @current-change="handlePageClick"
             background
         ></el-pagination>
     </div>
@@ -35,18 +60,38 @@
 export default {
     data() {
         return {
+            // 消息列表
             total: 0,
             totalPage: 1,
             pageNum: 1,
             pageSize: 8,
             list: [],
+            // 具体消息
+            dialogVisible: false,
+            receiverId: 0,
+            MuserName: "",
+            Mtotal: 0,
+            MtotalPage: 1,
+            MpageNum: 1,
+            MpageSize: 20,
+            Mlist: [],
+            content: ""
         }
     },
     mounted() {
-        this.getInform(this.pageNum, this.pageSize)
+        this.getMessageList(this.pageNum, this.pageSize)
+    },
+    computed: {
+        myName: {
+            get() {
+                return this.$store.state.username
+            },
+            set: function () {
+            }
+        }
     },
     methods: {
-        getInform(pageNum, pageSize) {
+        getMessageList(pageNum, pageSize) {
             let url = `/api/mms/msgList?pageNum=${pageNum}&pageSize=${pageSize}`; // https://bbs.fitymistudio.cn/api/mms/msgList
 
             this.axios
@@ -62,15 +107,78 @@ export default {
                 })
                 .catch((error) => console.log(error));
         },
-        handleCurrentChange(nextPage) {
-            this.getInform(nextPage, this.pageSize)
+        getDialogue(MuserName, userId, MpageNum, MpageSize) {
+            this.receiverId = userId
+            this.MuserName = MuserName
+
+            let url = `/api/mms/msgList?pageNum=${MpageNum}&pageSize=${MpageSize}&userId=${userId}`; // https://bbs.fitymistudio.cn/api/mms/msgList
+
+            this.axios
+                .get(url)
+                .then((response) => {
+                    let res = response.data.data
+                    console.log(res);
+
+                    this.Mlist = this.Mlist.concat(res.list)
+                    this.Mtotal = res.total
+                    this.MtotalPage = parseInt(res.totalPage)
+                    this.MpageNum++
+                })
+                .catch((error) => console.log(error));
+        },
+        handlePageClick(nextPage) {
+            this.getMessageList(nextPage, this.pageSize)
+
+        },
+        clearM(done) {
+            this.Mlist = []
+            this.receiverId = 0
+            this.MuserName = ""
+            this.Mtotal = 0
+            this.MtotalPage = 1
+            this.MpageNum = 1
+            this.Mlist = []
+            this.content = ""
+            done()
+        },
+        handleMPageClick() {
+            console.log(this.MuserName, this.receiverId, this.MpageNum, this.MpageSize);
+            this.getDialogue(this.MuserName, this.receiverId, this.MpageNum, this.MpageSize)
+        },
+        sendMessage() {
+            let url = `/api/mms/msg`; // https://bbs.fitymistudio.cn/api/mms/msg
+
+            let msg = {
+                receiverId: this.receiverId,
+                content: this.content
+            }
+            this.axios
+                .post(url, msg)
+                .then((response) => {
+                    this.$message(response.data.message)
+                })
+                .catch((error) => console.log(error));
         }
     }
 }
 </script>
 
-<style>
+<style >
 .imessage {
     margin: 20px;
+}
+
+.dialogue {
+    max-height: 60vh;
+    overflow-y: auto;
+    margin-bottom: 20px;
+}
+
+.infinite-list {
+    height: 300px;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    text-align: left;
 }
 </style>
