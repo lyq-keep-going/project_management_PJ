59147afc2553599ba59f7652156d5319ebab2d04
bathtown <hyf-0418@outlook.com>
2021-12-10 11:19:14 +0800

    add 退出

--- web/happy_go/src/pages/personal/Information.vue
+++ web/happy_go/src/pages/personal/Information.vue
@@ -2,6 +2,7 @@
     <div class="content">
         <el-avatar shape="square" :size="80">user.name</el-avatar>
         <p>user.name</p>
+        <el-button type="primary" @click="quit()">退出</el-button>
     </div>
 </template>
 <script>
@@ -11,8 +12,13 @@ export default {
         return {}
     },
     methods: {
-
-    },
+        quit() {
+            localStorage.removeItem("flag")
+            localStorage.removeItem("token")
+            localStorage.removeItem("tokenHead")
+            this.$store.commit("userLogout")
+        },
+    }
 }
 </script>
 
