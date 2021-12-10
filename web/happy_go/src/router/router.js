f706ab21cf77a356804d61f5ef5a509cefbbf1ba
bathtown <hyf-0418@outlook.com>
2021-12-10 11:19:41 +0800

    refine router

--- web/happy_go/src/router/router.js
+++ web/happy_go/src/router/router.js
@@ -10,22 +10,28 @@ import Home from '../pages/Home.vue'
 
 import Course from '../pages/Courses.vue'
 
-
 // 路由
 const routes = [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/personalCenter', component: PersonalCenter }
-
-
-    ,{ path: '/course', component: Course }
-
+    { path: '/', component: Home, meta: { isLogin: false } },
+    { path: '/login', component: Login, meta: { isLogin: false } },
+    { path: '/register', component: Register, meta: { isLogin: false } },
+    { path: '/personalCenter', component: PersonalCenter, meta: { isLogin: true } },
+    { path: '/course', component: Course }
 ]
 
 const router = createRouter({
     routes: routes,
-    history: createWebHashHistory()
+    history: createWebHashHistory(),
+    created: function () {
+        if (!this.$route.matched.length) {
+            this.$router.push("/");
+        }
+        let getFlag = localStorage.getItem("flag");
+        let getToken = localStorage.getItem("token");
+        if ((getToken === null || getFlag !== "isLogin") && this.$route.path != '/login') {
+            this.$router.push("/login");
+        }
+    },
 })
 
 export default router
