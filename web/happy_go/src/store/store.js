3f5432f5858339e21bcedea4b37b03f731481035
bathtown <hyf-0418@outlook.com>
2021-12-10 11:19:27 +0800

    add store

---
+++ web/happy_go/src/store/store.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: { isLogin: false, },
+    mutations: {
+        userLogin(state) { state.isLogin = true },
+        userLogout(state) { state.isLogin = false },
+    },
+
+    // 应用mutations
+    actions: {
+        //获取登录状态
+        setUser({ commit }, flag) { commit("userStatus", flag) },
+    }
+})
+
+export default store
