0ddc47ba3417ad18bd4dbc8ff4849db1295a49c6
bathtown <hyf-0418@outlook.com>
2021-12-13 22:33:02 +0800

    finish 收藏的课程

--- web/happy_go/src/components/Course.vue
+++ web/happy_go/src/components/Course.vue
@@ -2,14 +2,27 @@
     <el-card class="courseCard" shadow="hover">
         <div class="block">
             <el-carousel>
-                <el-carousel-item v-for="item in 4" :key="item">
-                    <img src="../assets/logo.png" />
+                <el-carousel-item v-for="pic in info.pictures" :key="pic">
+                    <img :src="pic" />
                 </el-carousel-item>
             </el-carousel>
         </div>
-        <div>
-            <h3>课程</h3>
-            <p>课程简介</p>
+
+        <h3>课程名称：{{ info.lessonName }}</h3>
+
+        <el-button type="primary" @click="dialogVisible = true">查看详细信息</el-button>
+        <div style="text-align: left;">
+            <el-dialog v-model="dialogVisible" title="详细信息" width="60%">
+                <div>
+                    <el-form label-width="80px" :model="info" label-position="left">
+                        <el-form-item label="课程名称">{{ info.lessonName }}</el-form-item>
+                        <el-form-item label="课程编号">{{ info.lessonNumber }}</el-form-item>
+                        <el-form-item label="授课老师">{{ info.teacherName }}</el-form-item>
+                        <el-form-item label="开课学期">{{ info.semester }}</el-form-item>
+                        <el-form-item label="课程学分">{{ info.credit }}</el-form-item>
+                    </el-form>
+                </div>
+            </el-dialog>
         </div>
     </el-card>
 </template>
@@ -17,7 +30,15 @@
 <script lang="ts">
 export default {
     data() {
-        return {}
+        return {
+            dialogVisible: false
+        }
+    },
+    props: {
+        info: {
+            type: Object,
+            default: () => { }
+        }
     },
     methods: {
 
@@ -29,5 +50,8 @@ export default {
 .courseCard {
     width: 100%;
     margin-top: 20px;
+}
+.courseCard img {
+    width: 100%;
 }
 </style>
