295add6774ad1dc700782191f0c9a76120f2a9d1
罗宇琦 <1258648285@qq.com>
2021-12-13 22:02:11 +0800

    discussion almost there, waiting for trade

--- mini-project/PJmanagement/pages/discussion/discussion.js
+++ mini-project/PJmanagement/pages/discussion/discussion.js
@@ -6,9 +6,126 @@ Page({
      * 页面的初始数据
      */
     data: {
-        tagnames:["可可爱爱","啦啦", "好耶"],
+        tags:[],
+        candidate_tags:[],
         entryList:[],
-        hide_add_tag : true
+        hide_add_tag : true,  
+        curr_page: 1,
+        last_page:1000,
+        lessonId:6,//假的，要改的
+        lessonInfo:{
+            id:6,
+            collected: true,
+            pictures: [
+                "https://url.cy/X3pCA3"
+            ],
+            lessonName: "项目管理",
+            lessonNumber: "SOFTXXXX",
+            teacherName: "高晓桐",
+            semester: "秋季",
+            credit: 5
+        }
+    },
+
+    enterPost(e){
+        wx.navigateTo({
+          url: '../post_detail/post_detail?topicId=' + e.target.dataset.topicid,
+        })
+        console.log(e.target.dataset);
+    },
+
+    getLessonInfo(){
+        wx.request({ 
+            url: 'https://bbs.fitymistudio.cn/api/lms/info?id=' + this.data.lessonId,
+            header:{
+                "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token
+            },
+            success:(result)=>{
+                console.log(result);
+                this.setData({
+                    lessonInfo: result.data.data
+                })
+            }
+        });
+    },
+    
+    addPost(e){
+        wx.navigateTo({
+          url: '../add_post/add_post?lessonId='+ this.data.lessonId,
+        })
+    },
+
+    sendPostRequest(pageNum, size, lessonId){
+        wx.request({ 
+            url: 'https://bbs.fitymistudio.cn/api/ums/topicList?lessonId='+ lessonId + '&pageNum=' + pageNum + '&pageSize=' + size,
+            header:{
+                "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token
+            },
+            success:(result)=>{
+                console.log(result)
+                this.setData({
+                    entryList: result.data.data.list,
+                    last_page: result.data.data.totalPage
+                })
+            }
+          });
+    },
+
+    getHotestTags(pageNum, pageSize, lessonId){
+        wx.request({ 
+            url: 'https://bbs.fitymistudio.cn/api/lms/tagList?pageNum='+ pageNum + '&pageSize=' + pageSize + '&lessonId=' + lessonId,
+            header:{
+                "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token
+            },
+            success:(result)=>{
+                this.setData({
+                    tags: result.data.data.list
+                })
+            }
+          });
+    },
+
+    getMoreTags(pageNum, pageSize, lessonId){
+        wx.request({ 
+            url: 'https://bbs.fitymistudio.cn/api/lms/tagList?pageNum='+ pageNum + '&pageSize=' + pageSize + '&lessonId=' + lessonId,
+            header:{
+                "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token
+            },
+            success:(result)=>{
+                this.setData({
+                    candidate_tags: result.data.data.list
+                })
+            }
+        });
+    },
+
+    onPreviousPage(e){
+        if(this.data.curr_page == 1){
+            wx.showToast({
+                title: '已经是第一页了',
+                icon: 'none'
+            })
+        }
+        var that = this;
+        this.setData({
+            curr_page: that.data.curr_page - 1
+        })
+        this.sendPostRequest(this.data.curr_page, 5);
+    },
+
+    onNextPage(e){
+        if(this.data.curr_page == this.data.last_page){
+            wx.showToast({
+                title: '已经是最后一页了', //弹框内容
+                icon: 'none',  //弹框模式
+                duration: 2000    //弹框显示时间
+            })
+        }
+        var that = this;
+        this.setData({
+            curr_page: that.data.curr_page + 1
+        })
+        this.sendPostRequest(this.data.curr_page, 5);
     },
 
     hideScreenHandler(e){
@@ -20,26 +137,18 @@ Page({
     openAddTagHandler(e){ 
         this.setData({ 
             hide_add_tag: false
-        })
+        });
+        this.getMoreTags(1, 20, this.data.lessonId);
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        wx.request({ 
-          url: 'https://bbs.fitymistudio.cn/api/ums/topicList?lessonId=SOFTXXXX&pageNum=2&pageSize=5',
-          header:{
-              "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token
-          },
-          success:(result)=>{
-              console.log(result)
-              this.setData({
-                  entryList: result.data.data.list
-              })
-          }
-        });
+        this.getLessonInfo();
+        this.sendPostRequest(1, 5);
+        this.getHotestTags(1, 10, this.data.lessonId);
     },
-
+ 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
