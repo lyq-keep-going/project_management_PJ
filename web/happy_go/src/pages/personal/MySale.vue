b3f92f091fdb67031106c41087bc8050426d695b
bathtown <hyf-0418@outlook.com>
2021-12-13 21:38:38 +0800

    finish 我的出售

--- web/happy_go/src/pages/personal/MySale.vue
+++ web/happy_go/src/pages/personal/MySale.vue
@@ -28,71 +28,35 @@ export default {
             pageSize: 8,
             totalPage: 5,
             total: 35,
-            list: [
-                {
-                    id: 540691,
-                    picture: "obj",
-                    bookName: "《 导现办建 》",
-                    author: "李芳",
-                    publisher: "世政气就年与政",
-                    newDegree: "半新",
-                    price: 22,
-                    unit: "¥",
-                    content: "市质第。",
-                    seller: {
-                        id: 61090,
-                        username: "黎霞",
-                        avatar: "http://dummyimage.com/80x80/#FF6600'"
-                    },
-                    lesson: {
-                        id: 982,
-                        lessonName: "又最管"
-                    }
-                },
-                {
-                    id: 66333,
-                    picture: "obj",
-                    bookName: "《 流争高多 》",
-                    author: "吕刚",
-                    publisher: "再史程则",
-                    newDegree: "半新",
-                    price: 37,
-                    unit: "¥",
-                    content: "速住点。",
-                    seller: {
-                        id: 40217,
-                        username: "潘磊",
-                        avatar: "http://dummyimage.com/80x80/#FF6600'"
-                    },
-                    lesson: {
-                        id: 98,
-                        lessonName: "如众果了向"
-                    }
-                }
-            ]
+            list: []
         }
     },
     components: {
         Goods
     },
     methods: {
-        getMySale() {
-            let url = `/api/cms/commodities?pageNum=${this.pageNum}&pageSize=${this.pageSize}`;
+        getMySale(pageNum, pageSize) {
+            let url = `/api/cms/commodities?pageNum=${pageNum}&pageSize=${pageSize}`;
 
             this.axios
                 .get(url)
                 .then((response) => {
                     let res = response.data.data
                     console.log(res);
+
+                    this.list = res.list
+                    this.total = res.total
+                    this.totalPage = parseInt(res.totalPage)
+                    this.pageNum = parseInt(res.pageNum)
                 })
                 .catch((error) => console.log(error));
         },
-        handlePageClick() {
-
+        handlePageClick(nextPage) {
+            this.getMySale(nextPage, this.pageSize)
         }
     },
     mounted() {
-        this.getMySale()
+        this.getMySale(this.pageNum, this.pageSize)
     },
 }
 </script>
