3022e55488698e2b14772770f01784bb1d94bbb8
罗宇琦 <1258648285@qq.com>
2021-12-30 00:02:51 +0800

    finished

--- mini-project/PJmanagement/pages/commodity_detail/commodity_detail.js
+++ mini-project/PJmanagement/pages/commodity_detail/commodity_detail.js
@@ -1,4 +1,5 @@
 // pages/commodity_detail/commodity_detail.js
+const app = getApp();
 Page({
 
     /**
@@ -18,17 +19,100 @@ Page({
             newDegree:"成色",
             price:"价格",
             coverPercentage:"笔记完整程度",
-            singlePrint: "是否单面打印",
+            singlePrintChinese: "是否单面打印",
             paperSize: "纸张大小",
             content:"描述",
             chapters:"章节"
-        }
+        },
+        isSoldByMe: false
     },
     
     goToChat(e){
         wx.navigateTo({
           url: '../chat/chat?sellerId='+ this.data.info.sellerId,
         })
+    },
+
+    soledOut(e){
+        if(this.data.info.type == 1){
+            //ppt
+            wx.request({
+                url: 'https://'+ app.globalData.host + '/api/cms/ppt',
+                header:{
+                    "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },  
+                method:'DELETE',
+                data:{
+                    id: this.data.info.id
+                },
+                success:(res)=>{
+                    console.log(res)
+                    if(res.data.code == 200){
+                        wx.showToast({
+                          title: '已成功将商品下架',
+                        })
+                    }else{
+                        wx.showToast({
+                          title: '下架失败',
+                          icon:'error'
+                        })
+                    }
+                }
+            })
+        }else if(this.data.info.type == 2){
+            //book
+            wx.request({
+                url: 'https://'+ app.globalData.host + '/api/cms/book',
+                header:{
+                    "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },  
+                method:'DELETE',
+                data:{
+                    id: this.data.info.id
+                },
+                success:(res)=>{
+                    console.log(res)
+                    if(res.data.code == 200){
+                        wx.showToast({
+                          title: '已成功将商品下架',
+                        })
+                    }else{
+                        wx.showToast({
+                          title: '下架失败',
+                          icon:'error'
+                        })
+                    }
+                }
+            })
+        }else if(this.data.info.type == 3){
+            //notes
+            wx.request({
+                url: 'https://'+ app.globalData.host + '/api/cms/notes',
+                header:{
+                    "Authorization" : app.globalData.userInfo.tokenHead + app.globalData.userInfo.token,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },  
+                method:'DELETE',
+                data:{
+                    id: this.data.info.id
+                },
+                success:(res)=>{
+                    console.log(res)
+                    if(res.data.code == 200){
+                        wx.showToast({
+                          title: '已成功将商品下架',
+                        })
+                    }else{
+                        wx.showToast({
+                          title: '下架失败',
+                          icon:'error'
+                        })
+                    }
+                }
+            })
+        }
     },
 
     /**
@@ -39,6 +123,12 @@ Page({
             info: JSON.parse(options.single_commodity) 
         })
         console.log(this.data.info)
+        var openId = wx.getStorageSync('openId');
+        if(openId  == this.data.info.seller.openId){
+            this.setData({
+                isSoldByMe:true
+            })
+        }
     },
 
     /**
